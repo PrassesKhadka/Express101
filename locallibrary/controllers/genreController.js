2e5a80ec89990b839588485274b47e243b2d90a2
Prasses Khadka <khadkaprasses@gmail.com>
2023-09-20 05:40:35 -0400

    Genre update added

--- locallibrary/controllers/genreController.js
+++ locallibrary/controllers/genreController.js
@@ -94,10 +94,37 @@ exports.genre_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Genre update form on GET.
 exports.genre_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre update GET");
+  const genre=await Genre.findById(req.params.id).exec()
+  res.render("genre_form",{
+    title:"Update Genre",
+    genre:genre
+  })
 });
 
 // Handle Genre update on POST.
-exports.genre_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre update POST");
-});
+exports.genre_update_post =[ 
+  body("name")
+  .trim()
+  .isLength({min:3})
+  .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors=validationResult(req)
+    const genre=new Genre({
+      _id:req.params.id,
+      name:req.body.name,
+    })
+
+    if(!errors.isEmpty())
+    {
+      res.render("genre_form",{
+        title:"Update Genre",
+        genre:genre,
+        errors:errors.array(),
+      })
+    }else{
+      await Genre.findByIdAndUpdate(req.params.id,genre)
+      res.redirect(genre.url)
+    }
+  })
+]
